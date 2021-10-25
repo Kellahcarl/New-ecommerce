c41d476813f10e8b40b13d8c34bc368ce8b12f48
Caleb Kellah <don@kellah.thejitu.com>
2021-10-25 16:42:01 +0300

    change var to lets where possible

--- js/index.js
+++ js/index.js
@@ -29,35 +29,35 @@ cartModalOverlay.addEventListener('click', (e) => {
 const addToCart = document.getElementsByClassName('add-to-cart');
 const productRow = document.getElementsByClassName('product-row');
 
-for (var i = 0; i < addToCart.length; i++) {
+for (let i = 0; i < addToCart.length; i++) {
   button = addToCart[i];
   button.addEventListener('click', addToCartClicked)
 }
 
 function addToCartClicked (event) {
   button = event.target;
-  var cartItem = button.parentElement;
-  var price = cartItem.getElementsByClassName('product-price')[0].innerText;
+  let cartItem = button.parentElement;
+  let price = cartItem.getElementsByClassName('product-price')[0].innerText;
   
-  var imageSrc = cartItem.getElementsByClassName('product-image')[0].src;
+  let imageSrc = cartItem.getElementsByClassName('product-image')[0].src;
   addItemToCart (price, imageSrc);
   updateCartPrice()
 }
 
 function addItemToCart (price, imageSrc) {
-  var productRow = document.createElement('div');
+  let productRow = document.createElement('div');
   productRow.classList.add('product-row');
-  var productRows = document.getElementsByClassName('product-rows')[0];
-  var cartImage = document.getElementsByClassName('cart-image');
+  let productRows = document.getElementsByClassName('product-rows')[0];
+  let cartImage = document.getElementsByClassName('cart-image');
   
-  for (var i = 0; i < cartImage.length; i++){
+  for (let i = 0; i < cartImage.length; i++){
     if (cartImage[i].src == imageSrc){
       alert ('This item has already been added to the cart')
       return;
     }
   }
   
-  var cartRowItems = `
+  let cartRowItems = `
   <div class="product-row">
         <img class="cart-image" src="${imageSrc}" alt="">
         <span class ="cart-price">${price}</span>
@@ -76,7 +76,7 @@ function addItemToCart (price, imageSrc) {
 
 // Remove products from cart
 const removeBtn = document.getElementsByClassName('remove-btn');
-for (var i = 0; i < removeBtn.length; i++) {
+for (let i = 0; i < removeBtn.length; i++) {
   button = removeBtn[i]
   button.addEventListener('click', removeItem)
 }
@@ -88,15 +88,15 @@ function removeItem (event) {
 }
 
 // update quantity input
-var quantityInput = document.getElementsByClassName('product-quantity')[0];
+const quantityInput = document.getElementsByClassName('product-quantity')[0];
 
-for (var i = 0; i < quantityInput; i++){
+for (let i = 0; i < quantityInput; i++){
   input = quantityInput[i]
   input.addEventListener('change', changeQuantity)
 }
 
 function changeQuantity(event) {
-  var input = event.target
+  let input = event.target
   if (isNaN(input.value) || input.value <= 0){
     input.value = 1
   }
@@ -106,13 +106,13 @@ function changeQuantity(event) {
 
 // update total price
 function updateCartPrice() {
-  var total = 0
+  let total = 0
   for (var i = 0; i < productRow.length; i += 2) {
     cartRow = productRow[i]
-  var priceElement = cartRow.getElementsByClassName('cart-price')[0]
-  var quantityElement = cartRow.getElementsByClassName('product-quantity')[0]
-  var price = parseFloat(priceElement.innerText.replace('$', ''))
-  var quantity = quantityElement.value
+  let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+  let quantityElement = cartRow.getElementsByClassName('product-quantity')[0]
+  let price = parseFloat(priceElement.innerText.replace('$', ''))
+  let quantity = quantityElement.value
   total = total + (price * quantity )
     
   }
@@ -132,7 +132,7 @@ purchaseBtn.addEventListener('click', purchaseBtnClicked)
 function purchaseBtnClicked () {
   alert ('Thank you for your purchase');
   cartModalOverlay.style.transform= 'translateX(-100%)'
- var cartItems = document.getElementsByClassName('product-rows')[0]
+ let cartItems = document.getElementsByClassName('product-rows')[0]
  while (cartItems.hasChildNodes()) {
    cartItems.removeChild(cartItems.firstChild)
    
@@ -144,30 +144,17 @@ function purchaseBtnClicked () {
 //alert user if cart is empty 
 
 
-var slideIndex = 0;
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
